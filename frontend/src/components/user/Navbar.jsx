a22262c6960e041bc6bf791953a089d4956591c9
dragonfxr <rfeng422@gmail.com>
2024-06-20 01:26:55 +1000

    stated frontend

---
+++ frontend/src/components/user/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="bg-red-200">
+      <div className="bg-black text-white max-w-screen-xl mx-auto">Hello</div>
+    </div>
+  );
+};
