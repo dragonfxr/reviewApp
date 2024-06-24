0847aef6e4e0a42cd499e8ad346609bf71ea5b80
dragonfxr <rfeng422@gmail.com>
2024-06-25 01:51:04 +1000

    finished otp

--- frontend/src/components/auth/EmailVerification.jsx
+++ frontend/src/components/auth/EmailVerification.jsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import Container from '../Container';
+import Submit from '../form/Submit';
+import Title from '../form/Title';
+
+const OTP_LENGTH = 6;
+let currentOTPIndex;
 
 export default function EmailVerification() {
+
+  const [otp, setOtp] = useState(new Array(OTP_LENGTH).fill(''));
+  const [activeOtpIndex, setActiveOtpIndex] = useState(0);
+  
+  const inputRef = useRef();
+
+  const focusNextInputField = (index) => {
+    setActiveOtpIndex(index + 1);
+  };
+
+  const focusPrevInputField = (index) => {
+    let nextIndex;
+    const diff = index - 1;
+    nextIndex = diff !== 0 ? diff : 0;
+    setActiveOtpIndex(nextIndex);
+  };
+
+  const handleOtpChange = ({ target }) => {
+    // everything will be same here just remove the index from 
+    // parameter and replace index with currentOTPIndex
+    const { value } = target;
+    const newOtp = [...otp];
+    newOtp[currentOTPIndex] = value.substring(value.length - 1, value.length);
+ 
+    if (!value) focusPrevInputField(currentOTPIndex);
+    else focusNextInputField(currentOTPIndex);
+ 
+    setOtp([...newOtp]);
+  };
+
+  const handleKeyDown = ({ key }, index) => {
+    currentOTPIndex = index;
+    if (key === "Backspace") {
+        focusPrevInputField(currentOTPIndex);
+     }
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [activeOtpIndex]);
+
   return (
-    <div>EmailVerification</div>
+    <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center'>
+        <Container>
+            <form className='bg-secondary rounded p-6 space-y-6'>
+              <div>
+                <Title>Please Enter the OPT to Verify</Title>
+                <p className='text-center text-dark-subtle'>An OTP has been sent to your email address</p>
+              </div>
+
+            <div className='flex justify-center items-center space-x-4'>
+              {otp.map((_, index) => {
+                return (
+                  <input
+                    ref={activeOtpIndex === index ? inputRef : null}
+                    key={index}
+                    type='number' 
+                    value={otp[index] || ''}
+                    onChange={handleOtpChange}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                    className='w-12 h-12 border-2 rounded border-dark-subtle focus:border-white bg-transparent outline-none
+                    text-center text-white font-semibold text-xl spin-button-none'/>
+                );
+              })}
+            </div>
+              <Submit value='Send Link'/>
+            </form>
+        </Container>
+    </div>
   )
 }
--- frontend/src/components/auth/ForgetPassword.jsx
+++ frontend/src/components/auth/ForgetPassword.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Container from '../Container';
+import Title from '../form/Title';
+import Submit from '../form/Submit';
+import CustomLink from '../CustomLink';
+import FormInput from '../form/FormInput';
 
 export default function ForgetPassword() {
   return (
-    <div>ForgetPassword</div>
-  )
-}
+    <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center'>
+        <Container>
+            <form className='bg-secondary rounded p-6 w-96 space-y-6'>
+                <Title>Please Enter Your Email</Title>
+                <FormInput label='Email' placeholder='myemail@email.com' name='email'/>
+                <Submit value='Send Link'/>
+                <div className="flex justify-between">
+                    <CustomLink to='/auth/signin'>Sign In</CustomLink>
+                    <CustomLink to='/auth/signup'>Sign Up</CustomLink>
+                </div>
+            </form>
+        </Container>
+    </div>
+  );
+};
